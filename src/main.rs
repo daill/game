e7a25686fa39fc292cafbf4a091cd92d38730e95
Christian Kramer <ck.daill@gmail.com>
2022-02-01 19:42:37 +0100

    transfer

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use std::process;
+
 fn main() {
     println!("Hello, world!");
+    // make sure that we're not running anything else then wasm32
+    #[cfg(not(target_arch == "wasm32"))]
+    process::exit(code: 0x0100);
+    #[cfg(target_arch == "wasm32")]
+    // create window which represents the browser
+
 }
