bcc22059ffe4d989de50f5212c72ebe4ddde241f
Christian Kramer <ck.daill@gmail.com>
2022-02-02 19:49:00 +0100

    transfer

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,17 @@
-extern crate console_error_panic_hook;
-use std::process;
-use winit::{
-    event::{Event, WindowEvent},
-    event_loop::{ControlFlow, EventLoop},
-    window::Window,
-};
+use log::{ info };
+
 
 fn main() {
     println!("Hello, world!");
+    
     // make sure that we're not running anything else then wasm32
     #[cfg(not(target_arch = "wasm32"))]
-    process::exit{code: 0x0100};
-    #[cfg(target_arch = "wasm32")]
+    std::process::exit(1);
+
     // create window which represents the browser
+    #[cfg(target_arch = "wasm32")]
     std::panic::set_hook(Box::new(console_error_panic_hook::hook));
     console_log::init().expect("could not initialize logger");
 
+    info!("test");
 }
