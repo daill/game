3a02a376bab9488e0af22b9e4a8ae0bd86cab0a7
Christian Kramer <ck.daill@gmail.com>
2022-02-02 20:59:15 +0100

    transfer

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 use log::{ info };
-
+use winit::{
+    event::{Event, WindowEvent},
+    event_loop::{ControlFlow, EventLoop},
+    window::Windows,
+};
 
 fn main() {
     println!("Hello, world!");
@@ -12,6 +16,10 @@ fn main() {
     #[cfg(target_arch = "wasm32")]
     std::panic::set_hook(Box::new(console_error_panic_hook::hook));
     console_log::init().expect("could not initialize logger");
+    let event_loop = EventLoop::new();
+    let window = winit::window::Window::new(event_loop: &event_loop).unwrap();
 
-    info!("test");
+    use winit::platform::web::WindowExtWebSys;
+
+
 }
