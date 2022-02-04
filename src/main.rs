3429030f6afdf8bd577a7174e4c1081ff02af311
Christian Kramer <ck.daill@gmail.com>
2022-02-04 09:48:43 +0100

    transfer

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
-use log::{ info };
+//use log::{ info };
 use winit::{
-    event::{Event, WindowEvent},
-    event_loop::{ControlFlow, EventLoop},
-    window::Windows,
+    //event::{Event, WindowEvent},
+    event_loop::{EventLoop},
+    //window::Window,
 };
 
 fn main() {
@@ -17,9 +17,15 @@ fn main() {
     std::panic::set_hook(Box::new(console_error_panic_hook::hook));
     console_log::init().expect("could not initialize logger");
     let event_loop = EventLoop::new();
-    let window = winit::window::Window::new(event_loop: &event_loop).unwrap();
+    let window = winit::window::Window::new(&event_loop).unwrap();
 
     use winit::platform::web::WindowExtWebSys;
 
+    web_sys::window().and_then(|win| win.document())
+        .and_then(|doc| doc.body())
+        .and_then(|body| {
+            body.append_child(&web_sys::Element::from(window.canvas())).ok()
+    }).expect("could not append canvas to body");
+    
 
 }
